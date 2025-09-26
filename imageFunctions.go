c1a9b7e53add17b23bc5ca5591b24ca25cc2b77b
CharacterHorse <github@rishay.fyi>
2025-09-27 03:37:44 +0530

    separate functions
    Signed-off-by: CharacterHorse <github@rishay.fyi>

---
+++ imageFunctions.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"image"
+	"image/jpeg"
+	"image/png"
+	"os"
+	"strings"
+)
+
+func DecodeImage(file *os.File, ext string) (image.Image, error) {
+	var decodedImage image.Image
+	var err error
+
+	switch ext {
+	case ".jpg", ".jpeg":
+		decodedImage, err = jpeg.Decode(file)
+	case ".png":
+		decodedImage, err = png.Decode(file)
+	}
+	if err != nil {
+		return nil, err
+	}
+	return decodedImage, nil
+}
+
+func OpenImage(filePath string) (image.Image, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	ext := strings.ToLower(filePath[strings.LastIndex(filePath, "."):])
+	return DecodeImage(file, ext)
+}
+
+func ToGray(img image.Image) *image.Gray {
+	bounds := img.Bounds()
+	newImg := image.NewGray(bounds)
+
+	for x := bounds.Min.X; x < bounds.Max.X; x++ {
+		for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+			originalColor := img.At(x, y)
+			newImg.Set(x, y, originalColor)
+		}
+	}
+
+	return newImg
+}
+
+func SaveImage(img image.Image, filePath string) error {
+	file, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	ext := strings.ToLower(filePath[strings.LastIndex(filePath, "."):])
+	switch ext {
+	case ".jpg", ".jpeg":
+		err = jpeg.Encode(file, img, &jpeg.Options{Quality: 100})
+	case ".png":
+		err = png.Encode(file, img)
+	}
+	return err
+}
